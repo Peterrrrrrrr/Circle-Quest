8d2104b3a16d8dd1b1fa11a43b891832bab9279d
Peter <77597544+Peterrrrrrrr@users.noreply.github.com>
2023-03-22 10:14:02 +0000

    circle now glows orange and fills with an orange colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
 import pygame
-import random
 
 # Initialize Pygame
 pygame.init()
@@ -15,20 +13,15 @@ clock = pygame.time.Clock()
 
 # Define colors
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+ORANGE = (255, 128, 0)
 
 # Set up the circle
-circle_radius = 30
-circle_border_width = 5
-circle_glow_speed = 5
-circle_glow_color = RED
-circle_color = BLACK
 circle_x = WIDTH // 2
 circle_y = HEIGHT // 2
-circle_speed = 5
+circle_radius = 50
+circle_fill_color = (255, 0, 0)
+circle_fill_color_index = 0
+circle_border_color = ORANGE
 
 # Set up the game loop
 running = True
@@ -37,32 +30,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
 
     # Handle circle movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and circle_x > circle_radius:
-        circle_x -= circle_speed
+        circle_x -= 5
     elif keys[pygame.K_RIGHT] and circle_x < WIDTH - circle_radius:
-        circle_x += circle_speed
-    if keys[pygame.K_UP] and circle_y > circle_radius:
-        circle_y -= circle_speed
+        circle_x += 5
+    elif keys[pygame.K_UP] and circle_y > circle_radius:
+        circle_y -= 5
     elif keys[pygame.K_DOWN] and circle_y < HEIGHT - circle_radius:
-        circle_y += circle_speed
+        circle_y += 5
 
-    # Handle circle glow animation
-    circle_glow_color = (circle_glow_color[1], circle_glow_color[2], circle_glow_color[0])
-    circle_color, circle_glow_color = circle_glow_color, circle_color
+    # Change the fill color of the circle
+    circle_fill_color_index += 1
+    if circle_fill_color_index >= 256:
+        circle_fill_color_index = 0
+    circle_fill_color = (circle_fill_color_index, 0, 0)
 
-    # Draw everything
-    screen.fill(WHITE)
-    pygame.draw.circle(screen, circle_glow_color, (circle_x, circle_y), circle_radius + circle_border_width, circle_border_width)
-    pygame.draw.circle(screen, circle_color, (circle_x, circle_y), circle_radius)
+    # Draw the screen
+    screen.fill(BLACK)
+    pygame.draw.circle(screen, circle_fill_color, (circle_x, circle_y), circle_radius)
+    pygame.draw.circle(screen, circle_border_color, (circle_x, circle_y), circle_radius, 5)
+
+    # Update the display
     pygame.display.update()
 
-    # Set the framerate
+    # Tick the clock
     clock.tick(60)
 
 # Quit Pygame
